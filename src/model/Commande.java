1e016fc1873520098565befed78d04f8a4f06db5
Sadrinho27 <Titouan.tabere@gmail.com>
2025-04-09 01:24:10 +0200

    Added :

--- src/model/Commande.java
+++ src/model/Commande.java
@@ -5,22 +5,51 @@ public class Commande {
 	private String nom;
 	private int qte;
 	private String statut;
-	private int roleUser; 
+	private int roleUser;
 	private String date;
-		
+
 	public Commande(String nom, int qte) {
 		this.nom = nom;
 		this.qte = qte;
 	}
-		
-	public String getNom() { return this.nom; }
-	public void setNom(String nom) { this.nom = nom; }
-	public int getQte() { return this.qte; }
-	public void setQte(int qte) { this.qte = qte; }
-	public String getStatut() { return this.statut; }
-	public void setStatut(String statut) { this.statut = statut; }
-	public int getRoleUser() { return this.roleUser; }
-	public void setRoleUser(int roleUser) { this.roleUser = roleUser; }
-	public String getDate() { return this.date; }
-	public void setDate(String date) { this.date = date; }
+
+	public String getNom() {
+		return this.nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getQte() {
+		return this.qte;
+	}
+
+	public void setQte(int qte) {
+		this.qte = qte;
+	}
+
+	public String getStatut() {
+		return this.statut;
+	}
+
+	public void setStatut(String statut) {
+		this.statut = statut;
+	}
+
+	public int getRoleUser() {
+		return this.roleUser;
+	}
+
+	public void setRoleUser(int roleUser) {
+		this.roleUser = roleUser;
+	}
+
+	public String getDate() {
+		return this.date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
 }
--- src/ui/PageConnexion.java
+++ src/ui/PageConnexion.java
@@ -1,22 +1,22 @@
 package ui;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 import dao.UtilisateurDAO;
 import model.Utilisateur;
 
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.JPasswordField;
-import java.awt.Color;
-import java.awt.Font;
- 
 public class PageConnexion extends JFrame {
 
 	private static final long serialVersionUID = 1L;
@@ -25,9 +25,9 @@ public class PageConnexion extends JFrame {
 	private JPasswordField passwordField;
 
 	public PageConnexion() {
-		
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(370,250,660,390);
+		setBounds(370, 250, 660, 390);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(255, 255, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -36,53 +36,53 @@ public class PageConnexion extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblLogin = new JLabel("Identifiant :");
 		lblLogin.setFont(new Font("Tahoma", Font.BOLD, 13));
 		lblLogin.setBounds(160, 94, 117, 20);
 		contentPane.add(lblLogin);
-		
+
 		textFieldLogin = new JTextField();
 		textFieldLogin.setBounds(160, 127, 319, 19);
 		contentPane.add(textFieldLogin);
 		textFieldLogin.setColumns(10);
-		
+
 		JLabel lblPassword = new JLabel("Mot de passe : ");
 		lblPassword.setFont(new Font("Tahoma", Font.BOLD, 13));
 		lblPassword.setBounds(160, 156, 134, 13);
 		contentPane.add(lblPassword);
-		
+
 		passwordField = new JPasswordField();
 		passwordField.setBounds(160, 179, 319, 19);
 		contentPane.add(passwordField);
-		
+
 		JButton btnConnexion = new JButton("Se connecter");
 		btnConnexion.setBackground(new Color(128, 128, 255));
 		btnConnexion.addActionListener(new ActionListener() {
-			
+
 			public void actionPerformed(ActionEvent e) {
-				
+
 				String login = textFieldLogin.getText();
-		        char[] password = passwordField.getPassword();
-		        textFieldLogin.setText(""); 
-		        passwordField.setText("");
+				char[] password = passwordField.getPassword();
+				textFieldLogin.setText("");
+				passwordField.setText("");
 
-		        Utilisateur user = UtilisateurDAO.seConnecter(login, new String(password));
+				Utilisateur user = UtilisateurDAO.seConnecter(login, new String(password));
 
-		        if (user != null) {
-		            JOptionPane.showMessageDialog(contentPane, "Vous êtes connecté.");
-		            PageAccueil accueil = new PageAccueil(user);
-		            accueil.setVisible(true);
-		            dispose();
-		        } else {
-		            JOptionPane.showMessageDialog(contentPane, "ERREUR | Identifiant ou mot de passe incorrect.", "Erreur connexion",
-		                    JOptionPane.ERROR_MESSAGE);
-		        }
+				if (user != null) {
+					JOptionPane.showMessageDialog(contentPane, "Vous êtes connecté.");
+					PageAccueil accueil = new PageAccueil(user);
+					accueil.setVisible(true);
+					dispose();
+				} else {
+					JOptionPane.showMessageDialog(contentPane, "ERREUR | Identifiant ou mot de passe incorrect.",
+							"Erreur connexion", JOptionPane.ERROR_MESSAGE);
+				}
 			}
 		});
 		btnConnexion.setBounds(160, 222, 319, 21);
 		contentPane.add(btnConnexion);
-		
+
 	}
-	
+
 }
