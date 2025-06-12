6169efeaaa166ca116bf90380bb63c0d9a3636a0
lemee <lemeematteo27@gmail.com>
2025-06-12 20:18:28 +0200

    update product model

--- src/model/Produit.java
+++ src/model/Produit.java
@@ -6,12 +6,14 @@ public class Produit {
 	private int codeProduit;
 	private String designationProduit;
 	private double prixPdt;
+	private int qte;
 	
-	public Produit(int id, int codeProduit, String designationProduit, double prixPdt) {
+	public Produit(int id, int codeProduit, String designationProduit, double prixPdt, int qte) {
 		this.id = id;
 		this.codeProduit = codeProduit;
 		this.designationProduit = designationProduit;
 		this.prixPdt = prixPdt;
+		this.qte = qte;
 	}
 	
 	public int getId() { return this.id; }
@@ -22,6 +24,7 @@ public class Produit {
 	public void setDesignationProduit(String designationProduit) { this.designationProduit = designationProduit; }
 	public double getPrix() { return this.prixPdt; }
 	public void setPrix(double prixPdt) { this.prixPdt = prixPdt; }
+	public int getQte() { return this.qte; }
 	
 	@Override
 	public String toString()
