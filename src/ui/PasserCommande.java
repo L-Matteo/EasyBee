f09452b232e51b2f29d95d860c2f776d670353d3
lemee <lemeematteo27@gmail.com>
2025-07-01 18:18:11 +0200

    refactor: update new order UI

--- src/ui/PasserCommande.java
+++ src/ui/PasserCommande.java
@@ -37,8 +37,8 @@ public class PasserCommande extends JFrame {
 	public PasserCommande() {
 		UIManager.put("Button.disabledText", Color.WHITE); // Obliger le bouton désactiver à avoir un text de couleur blanc
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(570, 250, 765, 465);
-		setTitle("Passer une commande - Gestion des stocks");
+		setBounds(440, 250, 1060, 620);
+		setTitle("Passer une commande | Gestion des stocks");
 		setResizable(false);
 		
 		contentPane = new JPanel();
@@ -49,60 +49,60 @@ public class PasserCommande extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lblTitle = new JLabel("Nouvelle commande d'approvisionnement");
-		lblTitle.setFont(new Font("Segoe UI", Font.BOLD, 18));
+		lblTitle.setFont(new Font("Segoe UI", Font.BOLD, 26));
 		lblTitle.setForeground(new Color(50, 50, 100));
-		lblTitle.setBounds(202, 20, 366, 25);
+		lblTitle.setBounds(285, 35, 560, 45);
 		contentPane.add(lblTitle);
 		
 		JLabel lblNomProduit = new JLabel("Nom du produit :");
-		lblNomProduit.setFont(new Font("Segoe UI", Font.BOLD, 14));
-		lblNomProduit.setBounds(88, 87, 158, 20);
+		lblNomProduit.setFont(new Font("Segoe UI", Font.BOLD, 20));
+		lblNomProduit.setBounds(120, 129, 248, 30);
 		contentPane.add(lblNomProduit);
 		
 		comboBoxProduit = new JComboBox<>();
-		comboBoxProduit.setBounds(88, 117, 234, 31);
-		comboBoxProduit.setFont(new Font("Segoe UI", Font.PLAIN, 13));
+		comboBoxProduit.setBounds(120, 176, 305, 31);
+		comboBoxProduit.setFont(new Font("Segoe UI", Font.PLAIN, 18));
 		contentPane.add(comboBoxProduit);
 		comboBoxProduit.addItem(new Produit(0,0,"— Sélectionnez un produit —",0,0));
 		
 		lblStock = new JLabel("Stock Entrepôt :");
-		lblStock.setBounds(88, 165, 226, 25);
-		lblStock.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		lblStock.setBounds(120, 228, 226, 25);
+		lblStock.setFont(new Font("Segoe UI", Font.PLAIN, 20));
 		contentPane.add(lblStock);
 		
 		JLabel lblQte = new JLabel("Quantité :");
-		lblQte.setFont(new Font("Segoe UI", Font.BOLD, 14));
-		lblQte.setBounds(88, 205, 158, 20);
+		lblQte.setFont(new Font("Segoe UI", Font.BOLD, 20));
+		lblQte.setBounds(120, 268, 158, 30);
 		contentPane.add(lblQte);
 		
 		textFieldQte = new JTextField();
-		textFieldQte.setFont(new Font("Segoe UI", Font.BOLD, 14));
-		textFieldQte.setBounds(88, 238, 234, 25);
+		textFieldQte.setFont(new Font("Segoe UI", Font.PLAIN, 18));
+		textFieldQte.setBounds(120, 315, 305, 35);
 		contentPane.add(textFieldQte);
 		textFieldQte.setColumns(10);
 		
 		btnAddProduit = new JButton("Ajouter à la commande");
 		btnAddProduit.setBackground(new Color(46, 204, 113));
 		btnAddProduit.setForeground(Color.WHITE);
-		btnAddProduit.setFont(new Font("Segoe UI", Font.BOLD, 13));
+		btnAddProduit.setFont(new Font("Segoe UI", Font.BOLD, 20));
 		btnAddProduit.setBorder(BorderFactory.createEmptyBorder(10, 15, 10, 15));
-		btnAddProduit.setBounds(88, 292, 234, 37);
+		btnAddProduit.setBounds(120, 375, 305, 50);
 		contentPane.add(btnAddProduit);
 		
 		btnRetour = new JButton("Retour");
 		btnRetour.setBackground(new Color(52, 152, 219));
 		btnRetour.setForeground(Color.WHITE);
-		btnRetour.setFont(new Font("Segoe UI", Font.BOLD, 13));
+		btnRetour.setFont(new Font("Segoe UI", Font.BOLD, 20));
 		btnRetour.setBorder(BorderFactory.createEmptyBorder(10, 15, 10, 15));
-		btnRetour.setBounds(219, 360, 93, 37);
+		btnRetour.setBounds(300, 482, 180, 50);
 		contentPane.add(btnRetour);
 		
 		btnValider = new JButton("Valider la commande");
 		btnValider.setBackground(new Color(100, 150, 255));
 		btnValider.setForeground(Color.WHITE);
-		btnValider.setFont(new Font("Segoe UI", Font.BOLD, 13));
+		btnValider.setFont(new Font("Segoe UI", Font.BOLD, 20));
 		btnValider.setBorder(BorderFactory.createEmptyBorder(10, 15, 10, 15));
-		btnValider.setBounds(337, 360, 189, 37);
+		btnValider.setBounds(517, 482, 250, 50);
 		contentPane.add(btnValider);
 		
 		model = new DefaultTableModel();
@@ -111,24 +111,25 @@ public class PasserCommande extends JFrame {
 		model.addColumn("Quantité");
 		
 		table = new JTable(model);
-		table.getColumnModel().getColumn(0).setPreferredWidth(60);  
-		table.getColumnModel().getColumn(1).setPreferredWidth(145); 
-		table.getColumnModel().getColumn(2).setPreferredWidth(60);  
+		table.getColumnModel().getColumn(0).setPreferredWidth(50); 
+		table.getColumnModel().getColumn(1).setPreferredWidth(175); 
+		table.getColumnModel().getColumn(2).setPreferredWidth(40);  
 		table.getColumnModel().getColumn(0).setResizable(false);
 		table.getColumnModel().getColumn(1).setResizable(false);
 		table.getColumnModel().getColumn(2).setResizable(false);
-		table.setRowHeight(21);
+		table.setRowHeight(31);
+		table.setFont(new Font("Segoe UI", Font.PLAIN, 18));
 		
 		JScrollPane scrollPane = new JScrollPane(table);
-		scrollPane.setBounds(378, 92, 322, 183);
+		scrollPane.setBounds(541, 129, 432, 221);
 		contentPane.add(scrollPane);
 		
 		btnSupProduit = new JButton("Supprimer le produit");
 		btnSupProduit.setBackground(new Color(220, 80, 80));
 		btnSupProduit.setForeground(Color.WHITE);
-		btnSupProduit.setFont(new Font("Segoe UI", Font.BOLD, 13));
+		btnSupProduit.setFont(new Font("Segoe UI", Font.BOLD, 20));
 		btnSupProduit.setBorder(BorderFactory.createEmptyBorder(10, 15, 10, 15));
-		btnSupProduit.setBounds(378, 292, 322, 37);
+		btnSupProduit.setBounds(541, 375, 432, 50);
 		contentPane.add(btnSupProduit);
 	}
 	
